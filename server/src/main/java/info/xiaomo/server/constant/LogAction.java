126cd95bd6754cff30df1f07d360902aa44eeafd
xiaomo <xiaomo@xiaomo.info>
2017-07-11 20:08:49 +0800

    logAction

---
+++ server/src/main/java/info/xiaomo/server/constant/LogAction.java
@@ -0,0 +1,25 @@
+package info.xiaomo.server.constant;
+
+public enum LogAction {
+
+    GM(1, "gm产生"),
+    ;
+
+    private int code;
+
+    private String comment;
+
+    LogAction(int code, String comment) {
+        this.code = code;
+        this.comment = comment;
+    }
+
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
