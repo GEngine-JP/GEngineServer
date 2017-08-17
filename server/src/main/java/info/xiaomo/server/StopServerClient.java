960af959cee045a79caa590e41fd97afc8785b69
xiaomo <xiaomo@xiaomo.info>
2017-08-17 08:04:10 +0800

    client

--- server/src/main/java/info/xiaomo/server/StopServerClient.java
+++ server/src/main/java/info/xiaomo/server/StopServerClient.java
@@ -1,13 +1,12 @@
 package info.xiaomo.server;
 
-import info.xiaomo.gameCore.protocol.AbstractHandler;
 import info.xiaomo.gameCore.protocol.MessagePool;
 import info.xiaomo.gameCore.protocol.client.Client;
 import info.xiaomo.gameCore.protocol.client.ClientBuilder;
-import info.xiaomo.gameCore.protocol.handler.MessageExecutor;
+import info.xiaomo.gameCore.protocol.protobuf.ProtoBufMessageDecoder;
+import info.xiaomo.gameCore.protocol.protobuf.ProtoBufMessageEncoder;
+import info.xiaomo.server.server.MessageRouter;
 import info.xiaomo.server.server.ServerOption;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,8 +26,9 @@ public class StopServerClient {
 
             builder.setHost("localhost");//只关本机的服务器
             builder.setPort(option.getBackServerPort());
-
-            builder.setMessagePool(new ClientMessagePool());
+            builder.setDecoder(new ProtoBufMessageDecoder(new ClientMessagePool()));
+            builder.setEncoder(new ProtoBufMessageEncoder(new ClientMessagePool()));
+            builder.setExecutor(new MessageRouter());
 
             Client client = builder.build();
             client.start();
@@ -45,32 +45,8 @@ public class StopServerClient {
 
     }
 
-    static class Consumer implements MessageExecutor {
 
-
-        @Override
-        public void doCommand(Channel channel, AbstractHandler handler) throws Exception {
-
-        }
-
-        @Override
-        public void connected(ChannelHandlerContext ctx) {
-
-        }
-
-        @Override
-        public void disconnected(ChannelHandlerContext ctx) {
-
-        }
-
-        @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable paramThrowable) {
-
-        }
-    }
-
-
-    static class ClientMessagePool extends MessagePool{
+    static class ClientMessagePool extends MessagePool {
 
 
     }
