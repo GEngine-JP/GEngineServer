b62273f96820e16bb11f6fda1dc790416ff1cfb3
xiaomo <xiaomo@xiaomo.info>
2017-08-24 17:40:26 +0800

    业务线程

--- server/src/main/java/info/xiaomo/server/processor/LogicProcessor.java
+++ server/src/main/java/info/xiaomo/server/processor/LogicProcessor.java
@@ -7,6 +7,9 @@ import info.xiaomo.server.server.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * 业务消息处理器
  *
@@ -16,26 +19,17 @@ import org.slf4j.LoggerFactory;
 public class LogicProcessor implements MessageProcessor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogicProcessor.class);
-
+    private Executor executor = Executors.newSingleThreadExecutor(r -> new Thread(r, ""));
 
     @Override
     public void process(IQueueDriverCommand message) {
 
         Session session = (Session) message.getParam();
-
-//        Player player = session.getPlayer();
-
-//		GameMap map = MapManager.getInstance().getMap(player.getMapId(), player.getLine());
-//
-//		if (map == null) { // 找不到地图
-//			return;
-//		}
-//		// 设置新的队列id为玩家地图id
-//		// 此处是为了防止
-//		message.setQueueId(player.getMapId());
-//
-//		map.getDriver().addCommand(message);
-
+        if (session == null) {
+            LOGGER.error("找不到session");
+            return;
+        }
+        this.executor.execute(message);
     }
 
 }
