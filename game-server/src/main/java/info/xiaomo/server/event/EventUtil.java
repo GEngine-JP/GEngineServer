83b4d708a536d3fea862f7a74d656fea92e68769
Evan Hu <kohou@ctw-inc.net>
2021-02-17 12:41:46 +0900

    upgrade to 3.1.4

--- game-server/src/main/java/info/xiaomo/server/event/EventUtil.java
+++ game-server/src/main/java/info/xiaomo/server/event/EventUtil.java
@@ -1,53 +1,52 @@
 package info.xiaomo.server.event;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 由于事件都是预先注册好的，所以这里不考虑多线程问题，不允许在游戏运行过成中动态添加观察者
  *
  * @author 张力
- * @date 2014-12-6 上午11:19:00
+ * 2014-12-6 上午11:19:00
  */
 public class EventUtil {
 
-    /**
-     * 游戏启动就初始化的监听者列表
-     */
-    private final static Map<EventType, List<IEventListener>> PREPARED_LISTENERS = new HashMap<>(10);
+	/**
+	 * 游戏启动就初始化的监听者列表
+	 */
+	private final static Map<EventType, List<IEventListener>> PREPARED_LISTENERS = new HashMap<>(10);
 
-    /**
-     * 游戏运行中动态添加的监听者列表
-     */
-    private final static Logger LOGGER = LoggerFactory.getLogger(EventUtil.class);
+	/**
+	 * 游戏运行中动态添加的监听者列表
+	 */
+	private final static Logger LOGGER = LoggerFactory.getLogger(EventUtil.class);
 
-    static void addListener(IEventListener listener, EventType type) {
-        List<IEventListener> listenerList = PREPARED_LISTENERS.computeIfAbsent(type, k -> new ArrayList<>());
-        listenerList.add(listener);
-    }
+	static void addListener(IEventListener listener, EventType type) {
+		List<IEventListener> listenerList = PREPARED_LISTENERS.computeIfAbsent(type, k -> new ArrayList<>());
+		listenerList.add(listener);
+	}
 
-    public static void fireEvent(EventType type) {
-        fireEvent(type, null);
-    }
+	public static void fireEvent(EventType type) {
+		fireEvent(type, null);
+	}
 
-    public static void fireEvent(EventType type, Object obj) {
+	public static void fireEvent(EventType type, Object obj) {
 
-        List<IEventListener> listenerList = PREPARED_LISTENERS.get(type);
-        if (listenerList != null) {
-            for (IEventListener listener : listenerList) {
-                try {
-                    listener.update(type, obj);
-                } catch (Exception e) {
-                    LOGGER.error("事件执行错误", e);
-                }
-            }
-        }
-    }
+		List<IEventListener> listenerList = PREPARED_LISTENERS.get(type);
+		if (listenerList != null) {
+			for (IEventListener listener : listenerList) {
+				try {
+					listener.update(type, obj);
+				} catch (Exception e) {
+					LOGGER.error("事件执行错误", e);
+				}
+			}
+		}
+	}
 
 
 }
